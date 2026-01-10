b168d612316dc51a388e1b67c9b24e36cbc2ed06
mateyct <masonctolley1@gmail.com>
2026-01-09 20:03:52 -0700

    feat: added equals logic for ChessPosition

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -8,7 +8,12 @@ package chess;
  */
 public class ChessPosition {
 
+    private int row;
+    private int col;
+
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
@@ -16,7 +21,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
@@ -24,6 +29,19 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof ChessPosition comparePos)) {
+            return false;
+        }
+        return row == comparePos.getRow() && col == comparePos.getColumn();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
     }
 }
