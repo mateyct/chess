749a820d61098ebdcc447f65ed56082e001635ee
mateyct <masonctolley1@gmail.com>
2026-01-10 14:38:43 -0700

    feat: implemented resetBoard on ChessBoard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -10,6 +10,23 @@ import java.util.Arrays;
  */
 public class ChessBoard {
 
+    /**
+     * The starting order of pieces in a game of chess, discounting pawns.
+     */
+    private static final ChessPiece.PieceType[] STARTING_PIECE_ORDER = {
+            ChessPiece.PieceType.ROOK,
+            ChessPiece.PieceType.KNIGHT,
+            ChessPiece.PieceType.BISHOP,
+            ChessPiece.PieceType.QUEEN,
+            ChessPiece.PieceType.KING,
+            ChessPiece.PieceType.BISHOP,
+            ChessPiece.PieceType.KNIGHT,
+            ChessPiece.PieceType.ROOK
+    };
+
+    /**
+     * The 2D array representing the grid of a chess board.
+     */
     private ChessPiece[][] boardGrid;
 
     public ChessBoard() {
@@ -23,7 +40,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-       boardGrid[position.getRow()][position.getColumn()] = piece;
+       boardGrid[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     /**
@@ -34,7 +51,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        return boardGrid[position.getRow()][position.getColumn()];
+        return boardGrid[position.getRow() - 1][position.getColumn() - 1];
     }
 
     /**
@@ -42,7 +59,34 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        ChessPiece[][] board = new ChessPiece[8][8];
+        addWhite(board);
+        addBlack(board);
+        boardGrid = board;
+    }
+
+    /**
+     * Adds white pieces on the board arranged for a new game.
+     * @param board the board to use.
+     */
+    private void addWhite(ChessPiece[][] board) {
+        ChessGame.TeamColor color = ChessGame.TeamColor.WHITE;
+        for (int i = 0; i < board[0].length; i++) {
+            board[0][i] = new ChessPiece(color, STARTING_PIECE_ORDER[i]);
+            board[1][i] = new ChessPiece(color, ChessPiece.PieceType.PAWN);
+        }
+    }
+
+    /**
+     * Adds black pieces on the board arranged for a new game.
+     * @param board the board to use.
+     */
+    private void addBlack(ChessPiece[][] board) {
+        ChessGame.TeamColor color = ChessGame.TeamColor.BLACK;
+        for (int i = 0; i < board[0].length; i++) {
+            board[7][i] = new ChessPiece(color, STARTING_PIECE_ORDER[i]);
+            board[6][i] = new ChessPiece(color, ChessPiece.PieceType.PAWN);
+        }
     }
 
     @Override
@@ -56,5 +100,28 @@ public class ChessBoard {
             return false;
         }
         return Arrays.deepEquals(boardGrid, compare.boardGrid);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder repr = new StringBuilder();
+        for (int i = 7; i >= 0; i--) {
+            for (ChessPiece piece : boardGrid[i]) {
+                String toAdd = " ";
+                if (piece != null) {
+                    switch (piece.getPieceType()) {
+                        case KING -> toAdd = "K";
+                        case PAWN -> toAdd = "P";
+                        case QUEEN -> toAdd = "Q";
+                        case KNIGHT -> toAdd = "N";
+                        case ROOK -> toAdd = "R";
+                        case BISHOP -> toAdd = "B";
+                    }
+                }
+                repr.append(toAdd);
+            }
+            repr.append("\n");
+        }
+        return repr.toString();
     }
 }
