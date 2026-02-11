7cf4deccc20aab7adbd02486b1781ea0e8910ec3
mateyct <masonctolley1@gmail.com>
2026-02-11 16:42:11 -0700

    feat: added AuthDAO interface

---
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -0,0 +1,11 @@
+package dataaccess;
+
+import model.AuthData;
+
+public interface AuthDAO {
+    public void clear();
+
+    public AuthData getAuth(String authToken);
+
+    public void removeAuth(String authToken);
+}
