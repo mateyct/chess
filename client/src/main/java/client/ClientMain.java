897767f8bda90e78b5c7ac1ca55b458c466acc02
mateyct <masonctolley1@gmail.com>
2026-03-13 11:35:00 -0600

    chore: start client REPL

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -2,8 +2,33 @@ package client;
 
 import chess.*;
 
+import java.util.Scanner;
+
 public class ClientMain {
-    public static void main(String[] args) throws InvalidMoveException {
+    private Scanner scan;
+
+    public static void main(String[] args) {
+        ClientMain client = new ClientMain();
+        client.loggedOutLoop();
+    }
+
+    public ClientMain() {
+        scan = new Scanner(System.in);
+        System.out.println("Welcome! It's time to play chess.");
+    }
+
+    private void loggedOutLoop() {
+        System.out.println(
+            """
+                1: Login
+                2: Register
+                3: Help
+                4: Quit"""
+        );
+
+    }
+
+    private static void drawTestBoard() {
         ClientChessBoard board = new ClientChessBoard();
         ChessGame game = new ChessGame();
         ChessMove move = new ChessMove(
@@ -11,7 +36,11 @@ public class ClientMain {
             new ChessPosition(3, 1),
             null
         );
-        game.makeMove(move);
+        try {
+            game.makeMove(move);
+        } catch (InvalidMoveException e) {
+            System.out.println("That's unfortunate.");
+        }
         board.draw(game.getBoard(), false);
         board.draw(game.getBoard(), true);
     }
