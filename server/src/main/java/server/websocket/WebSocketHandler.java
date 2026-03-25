0bca319801ce530fa50069808fce596a4a1821f1
mateyct <masonctolley1@gmail.com>
2026-03-24 20:49:44 -0600

    feat: connecting with notification working

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -2,8 +2,17 @@ package server.websocket;
 
 import io.javalin.websocket.*;
 import org.jetbrains.annotations.NotNull;
+import util.JSONTranslator;
+import websocket.commands.UserGameCommand;
+import websocket.messages.NotificationServerMessage;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+    private final JSONTranslator translator = new JSONTranslator();
 
     @Override
     public void handleConnect(@NotNull WsConnectContext ctx) {
@@ -13,12 +22,21 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
 
     @Override
     public void handleClose(@NotNull WsCloseContext ctx) {
-
+        System.out.println("WebSocket closed");
     }
-
 
     @Override
     public void handleMessage(@NotNull WsMessageContext ctx) {
-
+        try {
+            UserGameCommand command = translator.translateObject(ctx.message(), UserGameCommand.class);
+            if (command.getCommandType() == UserGameCommand.CommandType.CONNECT) {
+                connections.addSession(command.getGameID(), ctx.session);
+                ServerMessage msg = new NotificationServerMessage(command.getAuthToken() + " joined the game.");
+                connections.broadcast(command.getGameID(), ctx.session, msg);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
